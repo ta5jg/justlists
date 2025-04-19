23b33d2174ef0c9cbf51e68183b3c40271ecbcab
ta5jg <irfangedik@gmail.com>
2025-04-19 20:48:34 +0300

    Add files via upload

---
+++ usdtg.justlist.ts
@@ -0,0 +1,21 @@
+export default {
+  name: "USDTg Official List",
+  timestamp: "2025-04-19T20:55:00+00:00",
+  version: {
+    major: 1,
+    minor: 0,
+    patch: 0
+  },
+  logoURI: "https://gateway.pinata.cloud/ipfs/bafkreidmrotlmuxjooihz4omwosupptt43apwu7ps5onuizpetdonp7f3u",
+  keywords: ["usdtg", "tetherground", "stablecoin", "tron"],
+  tokens: [
+    {
+      chainId: 728126428,
+      address: "TEpGiLGNB7W9W26LbvtU9wdukrLgLZwFgr",
+      name: "TetherGround USD",
+      symbol: "USDTg",
+      decimals: 6,
+      logoURI: "https://gateway.pinata.cloud/ipfs/bafkreidmrotlmuxjooihz4omwosupptt43apwu7ps5onuizpetdonp7f3u"
+    }
+  ]
+};
